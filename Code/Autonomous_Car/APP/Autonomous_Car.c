ffe39c5a6fef5012f7ae42c015fb55c455af07d4
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2023-12-02 01:09:25 +0200

    First Push

---
+++ Code/Autonomous_Car/APP/Autonomous_Car.c
@@ -0,0 +1,17 @@
+/*
+ * Autonomous_Car.c
+ *
+ * Created: 02-Dec-23 12:48:25 AM
+ *  Author: lenovo
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
